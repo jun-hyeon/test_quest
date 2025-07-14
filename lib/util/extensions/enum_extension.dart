d39224f51fdbb77fc7e964b57fb7feec8bc51f4e
jun-hyeon <cjhn8918989@gmail.com>
2025-07-14 14:11:39 +0900

    enum toString추가

---
+++ lib/util/extensions/enum_extension.dart
@@ -0,0 +1,24 @@
+import 'package:test_quest/community/model/test_post.dart';
+
+extension TestTypeExtension on TestType {
+  String toPostString() {
+    return switch (this) {
+      TestType.cbt => 'cbt',
+      TestType.obt => 'obt',
+      TestType.alpha => 'alpha',
+      TestType.beta => 'beta',
+      TestType.unknown => 'unknown',
+    };
+  }
+}
+
+extension TestPlatformExtension on TestPlatform {
+  String toPostString() {
+    return switch (this) {
+      TestPlatform.console => 'console',
+      TestPlatform.mobile => 'mobile',
+      TestPlatform.pc => 'pc',
+      TestPlatform.unknown => 'unknown'
+    };
+  }
+}
