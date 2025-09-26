1d67d1f1c6785c62860e6f61c527ea390e0da06a
jun-hyeon <cjhn8918989@gmail.com>
2025-09-26 12:39:13 +0900

    버튼 수정

--- lib/common/component/custom_button.dart
+++ lib/common/component/custom_button.dart
@@ -6,18 +6,22 @@ class CustomButton extends StatelessWidget {
     required this.onPressed,
     required this.child,
     this.width,
+    this.shape,
   });
 
   final VoidCallback? onPressed;
   final double? width;
   final Widget child;
+  final OutlinedBorder? shape;
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       width: width ?? double.infinity,
       child: ElevatedButton(
-        style: ElevatedButton.styleFrom(),
+        style: ElevatedButton.styleFrom(
+          shape: shape,
+        ),
         onPressed: onPressed,
         child: child,
       ),
